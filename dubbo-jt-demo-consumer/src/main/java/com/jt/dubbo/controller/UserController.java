3ddc9d9986a3867e418b05d1d92bb8771d42adf2
Administrator <Administrator@QH-20190826ZHQZ>
2020-01-08 21:13:07 +0800

    dubbo

---
+++ dubbo-jt-demo-consumer/src/main/java/com/jt/dubbo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.jt.dubbo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jt.dubbo.pojo.User;
+import com.jt.dubbo.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Reference(timeout=3000,check=false)
+	private UserService userService;
+	
+	@RequestMapping("/findAll")
+	public List<User> findAll(){
+		
+		return userService.findAll();
+	}
+	
+	@RequestMapping("/saveUser/{name}/{age}/{sex}")
+	public String saveUser(User user) {
+		
+		userService.saveUser(user);
+		return "用户入库成功!!!";
+	}
+}
